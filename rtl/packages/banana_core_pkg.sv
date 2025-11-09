ab1ee62e3066b9778bbca4cd4e06ab8045d8c305
Денис Панин <lozejor12@gmail.com>
2025-11-09 16:38:43 +0300

    Feat[dev_bc0] Add parameters for instruction memory into package

--- rtl/packages/banana_core_pkg.sv
+++ rtl/packages/banana_core_pkg.sv
@@ -1,8 +1,12 @@
 package banana_core_pkg
 
-int unsigned DATA_WIDTH    = 32;
+int unsigned DATA_WIDTH  = 32;
+int unsigned INSTR_WIDTH = 32;
 
 int unsigned RF_REGS_COUNT = 32;
 int unsigned RF_ADDR_WIDTH = $clog2(RF_REGS_COUNT);
+
+int unsigned INSTR_MEM_SIZE       = 1024;  // count of INSTR_WIDTH bits instructions
+int unsigned INSTR_MEM_ADDR_WIDTH = $clog2(INSTR_MEM_SIZE);
 
 endpackage
