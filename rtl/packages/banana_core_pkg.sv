85abb9f327c28d63594354dce794f1e565c3b7c3
SinDrag0n <vladislavbelyaev04@gmail.com>
2025-11-19 23:36:01 +0300

    Feat[dev-compute_unit] Added compute-unit package

--- rtl/packages/banana_core_pkg.sv
+++ rtl/packages/banana_core_pkg.sv
@@ -5,4 +5,15 @@ int unsigned DATA_WIDTH    = 32;
 int unsigned RF_REGS_COUNT = 32;
 int unsigned RF_ADDR_WIDTH = $clog2(RF_REGS_COUNT);
 
+typedef struct packed {
+  // Register operands
+  logic [DATA_WIDTH - 1:0] rs1;
+  logic [DATA_WIDTH - 1:0] rs2;
+
+  // Immediates
+  logic [DATA_WIDTH - 1:0] i_imm;
+  logic [DATA_WIDTH - 1:0] u_imm;
+  logic [DATA_WIDTH - 1:0] s_imm;
+} comp_unit_data_t;
+
 endpackage
