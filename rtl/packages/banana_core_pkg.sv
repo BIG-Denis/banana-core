47fcc637fbb0941116f5320d9768f579d9bfd1c1
SinDrag0n <vladislavbelyaev04@gmail.com>
2026-01-07 16:22:18 +0200

    Feat[dev-compute_unit] Changed Compute unit to compute_alu

--- rtl/packages/banana_core_pkg.sv
+++ rtl/packages/banana_core_pkg.sv
@@ -5,18 +5,34 @@ int unsigned DATA_WIDTH    = 32;
 int unsigned RF_REGS_COUNT = 32;
 int unsigned RF_ADDR_WIDTH = $clog2(RF_REGS_COUNT);
 
-typedef struct packed {
-  // Register operands
-  logic [DATA_WIDTH - 1:0] rs1;
-  logic [DATA_WIDTH - 1:0] rs2;
+int unsigned ALU_OP1_COUNT = 2;
+int unsigned ALU_OP1_SEL_WIDTH = $clog2(ALU_OP2_COUNT);
 
-  // Immediates
-  logic [DATA_WIDTH - 1:0] i_imm;
-  logic [DATA_WIDTH - 1:0] u_imm;
-  logic [DATA_WIDTH - 1:0] s_imm;
+int unsigned ALU_OP2_COUNT = 6;
+int unsigned ALU_OP2_SEL_WIDTH = $clog2(ALU_OP2_COUNT);
 
-  // PC value
-  logic [DATA_WIDTH - 1:0] pc;
-} comp_unit_data_t;
+int unsigned OPCODE_WIDTH = 5;
+
+typedef enum logic [ALU_OP1_SEL_WIDTH - 1:0] {RS1, CONST4} op1_sel_t;
+typedef enum logic [ALU_OP2_SEL_WIDTH - 1:0] {RS2, PC, I_IMM, S_IMM, U_IMM, CONST0} op2_sel_t;
+
+typedef enum logic [OPCODE_WIDTH - 1:0] {
+  ADD,  
+  SUB,  
+  XOR,  
+  OR,   
+  AND,  
+  SRA,  
+  SRL,  
+  SLL,  
+  SLTS,
+  SLTU,
+  LTS,
+  LTU,
+  GES,
+  GEU,
+  EQ, 
+  NE
+} alu_opcode_t;
 
 endpackage
