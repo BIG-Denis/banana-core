73eac7d80f42420b7896871f466e89bced6011a3
SinDrag0n <vladislavbelyaev04@gmail.com>
2026-01-07 16:27:25 +0200

    Feat[dev-compute_unit] Added compare_alu module

---
+++ rtl/compare_alu.v
@@ -0,0 +1,30 @@
+module compare_alu
+import banana_core_pkg::*;
+(
+  // Control signals
+  input  dec_ctrls_t              dec_ctrls,
+
+  // RF ports
+  input  logic [DATA_WIDTH - 1:0] rs1_i,
+  input  logic [DATA_WIDTH - 1:0] rs2_i,
+
+  // Output port
+  output logic                    flag_o
+);
+
+logic flag;
+
+always_comb begin : compare_block
+  case ( dec_ctrls.alu_opcode ) 
+    LTS: flag = $signed( rs1_i ) < $signed( rs2_i );
+    LTU: flag = rs1_i < rs2_i;
+    GES: flag = $signed( rs1_i ) >= $signed( rs2_i );
+    GEU: flag = rs1_i >= rs2_i;
+    EQ:  flag = rs1_i == rs2_i;
+    NE:  flag = rs1_i != rs2_i;
+  endcase
+end : compare_block
+
+assign flag_o = flag;
+
+endmodule
