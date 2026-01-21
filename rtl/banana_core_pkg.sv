4944bce2a77f0665c848d89580a47ed4bd97da4b
SinDrag0n <vladislavbelyaev04@gmail.com>
2026-01-21 22:40:01 +0300

    Fix[dev-compute_unit]: fixed parameter name

--- rtl/banana_core_pkg.sv
+++ rtl/banana_core_pkg.sv
@@ -11,12 +11,12 @@ parameter int unsigned ALU_OP1_SEL_WIDTH = $clog2(ALU_OP2_COUNT);
 parameter int unsigned ALU_OP2_COUNT = 5;
 parameter int unsigned ALU_OP2_SEL_WIDTH = $clog2(ALU_OP2_COUNT);
 
-parameter int unsigned OPCODE_WIDTH = 5;
+parameter int unsigned ALU_OPCODE_WIDTH = 5;
 
 typedef enum logic [ALU_OP1_SEL_WIDTH - 1:0] {RS1, CONST4, U_IMM} alu_op1_sel_t;
 typedef enum logic [ALU_OP2_SEL_WIDTH - 1:0] {RS2, PC, I_IMM, S_IMM, CONST0} alu_op2_sel_t;
 
-typedef enum logic [OPCODE_WIDTH - 1:0] {
+typedef enum logic [ALU_OPCODE_WIDTH - 1:0] {
   ADD,  SUB,  XOR, OR,
   AND,  SRA,  SRL, SLL,
   SLTS, SLTU, LTS, LTU,
