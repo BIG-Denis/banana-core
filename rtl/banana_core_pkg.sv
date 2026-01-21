4939c99cf1e3decd7aab7856703aaf8eabdd211e
SinDrag0n <vladislavbelyaev04@gmail.com>
2026-01-21 22:16:15 +0300

    Fix[dev]: Moved pkg file, codestyle fix, changed operands enums

--- rtl/banana_core_pkg.sv
+++ rtl/banana_core_pkg.sv
@@ -13,26 +13,14 @@ int unsigned ALU_OP2_SEL_WIDTH = $clog2(ALU_OP2_COUNT);
 
 int unsigned OPCODE_WIDTH = 5;
 
-typedef enum logic [ALU_OP1_SEL_WIDTH - 1:0] {RS1, CONST4} op1_sel_t;
-typedef enum logic [ALU_OP2_SEL_WIDTH - 1:0] {RS2, PC, I_IMM, S_IMM, U_IMM, CONST0} op2_sel_t;
+typedef enum logic [ALU_OP1_SEL_WIDTH - 1:0] {RS1, CONST4, U_IMM} op1_sel_t;
+typedef enum logic [ALU_OP2_SEL_WIDTH - 1:0] {RS2, PC, I_IMM, S_IMM, CONST0} op2_sel_t;
 
 typedef enum logic [OPCODE_WIDTH - 1:0] {
-  ADD,  
-  SUB,  
-  XOR,  
-  OR,   
-  AND,  
-  SRA,  
-  SRL,  
-  SLL,  
-  SLTS,
-  SLTU,
-  LTS,
-  LTU,
-  GES,
-  GEU,
-  EQ, 
-  NE
+  ADD,  SUB,  XOR, OR,
+  AND,  SRA,  SRL, SLL,
+  SLTS, SLTU, LTS, LTU,
+  GES,  GEU,  EQ,  NE
 } alu_opcode_t;
 
 endpackage
