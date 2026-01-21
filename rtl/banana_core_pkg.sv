19a341126c82dbcb52f19b5977c67666b9c017b4
SinDrag0n <vladislavbelyaev04@gmail.com>
2026-01-21 22:33:19 +0300

    Fix[dev-compute_unit]: fixed syntax errors, changed parameters values

--- rtl/banana_core_pkg.sv
+++ rtl/banana_core_pkg.sv
@@ -1,17 +1,17 @@
-package banana_core_pkg
+package banana_core_pkg;
 
-int unsigned DATA_WIDTH    = 32;
+parameter int unsigned DATA_WIDTH    = 32;
 
-int unsigned RF_REGS_COUNT = 32;
-int unsigned RF_ADDR_WIDTH = $clog2(RF_REGS_COUNT);
+parameter int unsigned RF_REGS_COUNT = 32;
+parameter int unsigned RF_ADDR_WIDTH = $clog2(RF_REGS_COUNT);
 
-int unsigned ALU_OP1_COUNT = 2;
-int unsigned ALU_OP1_SEL_WIDTH = $clog2(ALU_OP2_COUNT);
+parameter int unsigned ALU_OP1_COUNT = 3;
+parameter int unsigned ALU_OP1_SEL_WIDTH = $clog2(ALU_OP2_COUNT);
 
-int unsigned ALU_OP2_COUNT = 6;
-int unsigned ALU_OP2_SEL_WIDTH = $clog2(ALU_OP2_COUNT);
+parameter int unsigned ALU_OP2_COUNT = 5;
+parameter int unsigned ALU_OP2_SEL_WIDTH = $clog2(ALU_OP2_COUNT);
 
-int unsigned OPCODE_WIDTH = 5;
+parameter int unsigned OPCODE_WIDTH = 5;
 
 typedef enum logic [ALU_OP1_SEL_WIDTH - 1:0] {RS1, CONST4, U_IMM} op1_sel_t;
 typedef enum logic [ALU_OP2_SEL_WIDTH - 1:0] {RS2, PC, I_IMM, S_IMM, CONST0} op2_sel_t;
